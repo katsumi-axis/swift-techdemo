f2e199d063fa3320c45ddb442e446fb9528ca099
katsumi <withcomputer@gmail.com>
2024-11-23 22:04:55 +0900

    Add platform support for iOS 16 and include new dependencies

--- Internal/Package.swift
+++ Internal/Package.swift
@@ -5,11 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "Internal",
+    platforms: [.iOS(.v16)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Internal",
             targets: ["Internal"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.6.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.16.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
