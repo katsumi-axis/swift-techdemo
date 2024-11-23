b9273cef808a73d7fff54584b624dc060d5ce8e5
katsumi <withcomputer@gmail.com>
2024-11-23 14:09:52 +0900

    Add initial project structure with assets

---
+++ iOS/swift_techdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_techdemoApp.swift
+//  swift-techdemo
+//
+//  Created by katsumi on 2024/11/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_techdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
